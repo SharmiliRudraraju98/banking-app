435a64378b429d28937aeae6631fc8174994ca47
Ponni-git <sharmivarmarudraraju@gmail.com>
2026-04-07 16:04:24 -0400

    feat: add Customer entity with JPA annotations - creates customers table

--- banking-fundtransfer/src/main/java/com/example/bankingfundtransfer/entity/Customer.java
+++ banking-fundtransfer/src/main/java/com/example/bankingfundtransfer/entity/Customer.java
@@ -1,4 +1,90 @@
 package com.example.bankingfundtransfer.entity;
+import jakarta.persistence.*;
+//import org.springframework.data.annotation.Id;
+//import lombok.Data;
+//
+//@Data
+@Entity
+@Table(name = "customers")
+
 
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+
+    @Column(name = "address_line1", nullable = false)
+    private String addressLine1;
+
+    @Column(name = "address_line2")
+    private String addressLine2;
+
+    @Column(nullable = false)
+    private String country;
+
+
+    public Customer(){
+
+    }
+
+    public Customer(String name, String email, String addressLine1, String addressLine2,String country){
+        this.name = name;
+        this.email =  email;
+        this.addressLine1 = addressLine1;
+        this.addressLine2 = addressLine2;
+        this.country = country;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
